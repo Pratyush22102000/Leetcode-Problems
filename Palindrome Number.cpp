54ed305c16c65ee2554739d868f47eddaf073a02
Pratyush22102000 <101665628+Pratyush22102000@users.noreply.github.com>
2022-10-16 12:53:50 +0530

    Update Palindrome Number.cpp

--- Palindrome Number.cpp
+++ Palindrome Number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
+        if(x < 0) return false;
         long int ans = 0;
-        int temp= x;
+        int temp = x;
         int remainder;
-        while(x!=0)
+        while(x != 0)
         {
-            remainder= x%10 ;
-            x = x/10;
-            ans=ans*10+remainder;
+            remainder = x % 10 ;
+            x = x / 10;
+            ans = ans * 10 + remainder ;
         }
-        if(temp==ans) return true;       
+        if(temp == ans) return true;       
         else return false;
             
         
